7a4bd223ebb4733162cc015af6514aeb3466f0cb
Frank Chiarulli Jr <frank@svix.com>
2021-05-28 18:26:18 -0400

    remove unused config code

--- cmd/root.go
+++ cmd/root.go
@@ -4,13 +4,9 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	svix "github.com/svixhq/svix-libs/go"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,10 +21,6 @@ func Execute() {
 }
 
 func init() {
-	// Set Config
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.svix.yaml)")
-	// cfg := cfg.New() // TODO initialize
 	key := os.Getenv("SVIX_KEY")
 	if key == "" {
 		fmt.Println("No SVIX_KEY found!")
@@ -46,28 +38,4 @@ func init() {
 	rootCmd.AddCommand(newEndpointCmd(s).Cmd())
 	rootCmd.AddCommand(newMessageCmd(s).Cmd())
 	rootCmd.AddCommand(newMessageAttemptCmd(s).Cmd())
-
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".svix" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".svix")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
