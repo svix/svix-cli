263e77c8038664968e0d9ad3f3e88966e08b397e
Frank Chiarulli Jr <frank@svix.com>
2021-06-02 19:19:20 -0400

    add completion function for generating shell completions

--- validators/validators.go
+++ validators/validators.go
@@ -37,17 +37,30 @@ func RangeArgs(min int, max int) cobra.PositionalArgs {
 func ExactArgs(n int) cobra.PositionalArgs {
 	return func(cmd *cobra.Command, args []string) error {
 		if len(args) != n {
-			msg := "`%s`requires %d args, received %d. Run `%s --help` for usage information."
 			if n == 1 {
-				msg = "`%s`requires 1 arg, received %d. Run `%s --help` for usage information."
+				return fmt.Errorf("`%s` requires 1 arg, received %d. Run `%s --help` for usage information.",
+					cmd.CommandPath(),
+					len(args),
+					cmd.CommandPath(),
+				)
+			} else {
+				return fmt.Errorf("`%s` requires %d args, received %d. Run `%s --help` for usage information.",
+					cmd.CommandPath(),
+					n,
+					len(args),
+					cmd.CommandPath(),
+				)
 			}
-			return fmt.Errorf(msg,
-				cmd.CommandPath(),
-				n,
-				len(args),
-				cmd.CommandPath(),
-			)
 		}
 		return nil
+	}
+}
+
+func ExactValidArgs(n int) cobra.PositionalArgs {
+	return func(cmd *cobra.Command, args []string) error {
+		if err := ExactArgs(n)(cmd, args); err != nil {
+			return err
+		}
+		return cobra.OnlyValidArgs(cmd, args)
 	}
 }
