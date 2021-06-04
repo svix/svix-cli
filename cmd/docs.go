8940646fc20e87714027647036a0a5a8f04be1f2
Dj <dj@codepope.com>
2021-06-04 11:53:07 +0100

    Remove lint issues - now produces error

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,16 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 	"github.com/svixhq/svix-cli/validators"
 )
 
 const docsURL = "https://docs.svix.com/"
-
-type docsCmd struct {
-	cmd *cobra.Command
-}
 
 func newDocsCmd() *versionCmd {
 	return &versionCmd{
@@ -19,7 +18,12 @@ func newDocsCmd() *versionCmd {
 			Args:  validators.NoArgs(),
 			Short: "Open the default browser with the Svix documentation",
 			Run: func(cmd *cobra.Command, args []string) {
-				open.Run(docsURL)
+				err := open.Run(docsURL)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err.Error())
+					fmt.Fprintln(os.Stderr, "Failed to open default application for URL.")
+					os.Exit(1)
+				}
 			},
 		},
 	}
