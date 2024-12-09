e8c1cdb6aa9697c8189a28eff578e0dc7df67f9a
Owen Nelson <onelson@svix.com>
2024-12-09 18:25:10 -0500

    stub out client creation, break up application into sub-sub commands

--- svix-cli2/src/main.rs
+++ svix-cli2/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[derive(Parser)]
 #[clap(color = concolor_clap::color_choice())]
 struct Cli {
@@ -9,22 +11,52 @@ struct Cli {
     command: Commands,
 }
 
+// N.b Ordering matters here for how clap presents the help.
+// FIXME: double-check Go cli. Seems like cobra may sort the items in the help lexigraphically
 #[derive(Subcommand)]
 enum Commands {
+    /// Get the version of the Svix CLI
     Version,
+    /// Interactively configure your Svix API credentials
     Login,
-    Application,
+    /// List, create & modify applications
+    Application(ApplicationArgs),
+    /// Manage authentication tasks such as getting dashboard URLs
     Authentication,
+    /// List, create & modify event types
     EventType,
+    /// List, create & modify endpoints
     Endpoint,
+    /// List & create messages
     Message,
+    /// List, lookup & resend message attempts
     MessageAttempt,
+    /// Verify the signature of a webhook message
     Verify,
+    /// Quickly open Svix pages in your browser
     Open,
+    /// Forward webhook requests a local url
     Listen,
+    /// Import data to your Svix Organization
     Import,
+    /// Export data from your Svix Organization
     Export,
+    /// List integrations by app id
     Integration,
+}
+
+#[derive(Subcommand)]
+enum ApplicationArgs {
+    /// List current applications
+    List,
+    /// Creates a new application
+    Create,
+    /// Get an application by id
+    Get,
+    /// Update an application by id
+    Update,
+    /// Deletes an application by id
+    Delete,
 }
 
 fn main() {
@@ -32,12 +64,18 @@ fn main() {
 
     match &cli.command {
         // Local-only things
-        Commands::Version => todo!("Commands::Version"),
+        Commands::Version => println!("{VERSION}"),
         Commands::Verify => todo!("Commands::Verify"),
         Commands::Open => todo!("Commands::Open"),
 
         // Remote API calls
-        Commands::Application => todo!("Commands::Application"),
+        Commands::Application(cmd) => match cmd {
+            ApplicationArgs::List => todo!("application list"),
+            ApplicationArgs::Create => todo!("application create"),
+            ApplicationArgs::Get => todo!("application get"),
+            ApplicationArgs::Update => todo!("application update"),
+            ApplicationArgs::Delete => todo!("application delete"),
+        },
         Commands::Authentication => todo!("Commands::Authentication"),
         Commands::EventType => todo!("Commands::EventType"),
         Commands::Endpoint => todo!("Commands::Endpoint"),
@@ -51,4 +89,17 @@ fn main() {
         Commands::Listen => todo!("Commands::Listen"),
         Commands::Login => todo!("Commands::Login"),
     }
+}
+
+fn get_client() -> svix::api::Svix {
+    // XXX: Go client will exit if the token is not set. May need to rewrangle the flow.
+    let token = String::new(); // FIXME: read from config
+    let opts = get_client_options();
+    svix::api::Svix::new(token, Some(opts))
+}
+
+fn get_client_options() -> svix::api::SvixOptions {
+    // FIXME: read options from config file
+    // FIXME: validate server url
+    svix::api::SvixOptions::default()
 }
